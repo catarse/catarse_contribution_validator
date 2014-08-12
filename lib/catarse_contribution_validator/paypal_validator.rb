10e244d93563085e784107f764960f541473304e
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-12 17:23:24 -0300

    fix paypal validator

--- lib/catarse_contribution_validator/paypal_validator.rb
+++ lib/catarse_contribution_validator/paypal_validator.rb
@@ -1,6 +1,6 @@
 module CatarseContributionValidator
   class PaypalValidator
-    attr_accessor :contribution, :current_details
+    attr_accessor :contribution, :current_details, :current_search_transaction, :search_transactions
 
     def initialize(contribution)
       self.contribution = contribution
@@ -14,15 +14,17 @@ module CatarseContributionValidator
     end
 
     def get_first_transaction
-     transaction = if search.params['PaymentTransactions'].present?
-                     search.params['PaymentTransactions'].select do |transaction|
-                       ((self.contribution.created_at - transaction['Timestamp'].to_datetime) / 1.hour) < 5.minutes &&
-                         self.contribution.created_at.to_date == transaction['Timestamp'].to_date &&
-                         self.contribution.value.to_f == transaction['GrossAmoun'].to_f.abs
-                     end
-                   else
-                     nil
-                   end
+      transaction = if search.params['PaymentTransactions'].present?
+                      if search.params['PaymentTransactions'].is_a?(Array)
+                        self.search_transactions = search.params['PaymentTransactions']
+                        search_on_array
+                      else
+                        self.current_search_transaction = search.params['PaymentTransactions']
+                        search_on_hash
+                      end
+                    else
+                      nil
+                    end
     end
 
     def in_paypal?
@@ -66,11 +68,35 @@ module CatarseContributionValidator
       puts "UPDATING TO PAYPAL"
       self.contribution.update_attributes({
         payment_method: CatarseContributionValidator::ServiceTypes::PAYPAL,
-        payment_service_fee: self.current_details.params['fee_amount'].to_f
+        payment_service_fee: self.current_details.params['fee_amount'].to_f.abs
       })
     end
 
     protected
+
+    def search_on_hash
+      if transaction_match?
+        self.current_search_transaction
+      else
+        nil
+      end
+    end
+
+    def search_on_array
+      self.search_transactions_list.params['PaymentTransactions'].select do |transaction|
+        self.current_search_transaction = transaction
+        transaction_match?
+      end
+    end
+
+    def transaction_match?
+      transaction_date = self.current_search_transaction['Timestamp'].to_datetime
+      transaction_date = transaction_date.in_time_zone("Brasilia")
+
+      ((self.contribution.created_at - transaction_date).abs / 1.hour) < 5.minutes && 
+        self.contribution.created_at.to_date == transaction_date.to_date && 
+        self.contribution.value.to_f == self.current_search_transaction['GrossAmount'].to_f.abs
+    end
 
     def gateway
       @gateway ||= CatarsePaypalExpress::Gateway.instance
