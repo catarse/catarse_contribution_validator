a5ce1407c402b3b3aba640aab54af6a8474e910a
Antônio Roberto <forevertonny@gmail.com>
2014-11-12 14:43:56 -0200

    get only payments made with paypal or moip

--- app/models/catarse_contribution_validator/contribution_concern.rb
+++ app/models/catarse_contribution_validator/contribution_concern.rb
@@ -3,7 +3,7 @@ module CatarseContributionValidator::ContributionConcern
 
   included do
     scope :collection_to_validate, -> {
-      where("(created_at + '1 month') > current_timestamp and payment_id is not null").
+      where("(created_at + '1 month') > current_timestamp and payment_id is not null and lower(payment_method) in ('paypal', 'moip')").
       with_state(['pending', 'waiting_confirmation', 'requested_refund', 'deleted']).
       order('created_at desc')
     }
